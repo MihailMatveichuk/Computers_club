629f108ceb849440b3d2939ffd7a76da6ec61c9e
Mikhail Matveichuk <mihkailmatveichuk@gmail.com>
2023-08-18 09:59:24 +0300

    feat: add Computer realization

--- Computers_club/Program.cs
+++ Computers_club/Program.cs
@@ -20,7 +20,44 @@ namespace Computers_club
 
     class Computer
     {
+        private SchoolBoy _schoolBoy;
+        private int _minutesLeft;
 
+        public int PriceForMinute { get; private set; }
+        public bool IsBusy
+        {
+            get
+            {
+                return _minutesLeft > 0;
+            }
+        }
+
+        public Computer(int priceForMinute)
+        {
+            PriceForMinute = priceForMinute;
+        }
+
+        public void TakeThePlace(SchoolBoy schoolBoy)
+        {
+            _schoolBoy = schoolBoy;
+            _minutesLeft = _schoolBoy.DesiredMinutes;
+        }
+
+        public void FreeThePlace()
+        {
+            _schoolBoy = null;
+        }
+
+        public void SkipMinutes()
+        {
+            _minutesLeft--;
+        }
+
+        public void ShowComputersInfo()
+        {
+            if (IsBusy) Console.WriteLine($"Computer is busy. {_minutesLeft} are left");
+            else Console.WriteLine($"Computer is free. Price for minute equals {PriceForMinute}");
+        }
     }
 
     class SchoolBoy
