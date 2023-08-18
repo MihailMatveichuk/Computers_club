f738b06df0de482b5970e5ad221d510597d72acd
Mikhail Matveichuk <mihkailmatveichuk@gmail.com>
2023-08-18 11:07:59 +0300

    feat: add ComputerClub realization

--- Computers_club/Program.cs
+++ Computers_club/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Schema;
 
 namespace Computers_club
 {
@@ -10,13 +11,110 @@ namespace Computers_club
     {
         static void Main(string[] args)
         {
+            ComputerClub computerClub = new ComputerClub(8);
+            computerClub.Work();
         }
     }
     class ComputerClub
     {
         private List<Computer> _computers = new List<Computer>();
         private Queue<SchoolBoy> _schoolBoys = new Queue<SchoolBoy>();
+        private int _money = 0;
+
+        public ComputerClub(int computersCount)
+        {
+            Random rand = new Random();
+
+            for (int i = 0; i < computersCount; i++)
+            {
+                _computers.Add(new Computer(rand.Next(5, 15)));
+            }
+            CreateNewSchoolBoys(25);
+        }
+        private void CreateNewSchoolBoys(int value)
+        {
+            Random rand = new Random();
+
+            for (int i = 0; i < value; i++)
+            {
+
+                _schoolBoys.Enqueue(new SchoolBoy(rand.Next(100, 250), rand));
+            }
+        }
+
+        public void Work()
+        {
+            while (_schoolBoys.Count > 0)
+            {
+                Console.WriteLine($"Our computer club have {_money} rubles now. We are waiting a new client!!!");
+
+                SchoolBoy schoolBoy = _schoolBoys.Dequeue();
+                Console.WriteLine($"The new young boy in queue. He wants to buy {schoolBoy.DesiredMinutes} minutes for play");
+
+                Console.WriteLine("\nList of computers: ");
+                ShowAllComputers();
+
+                Console.Write("\nWe are offering him PC under number - ");
+                try
+                {
+                    int computerNumber = Convert.ToInt32(Console.ReadLine()) - 1;
+                    if (computerNumber >= 0 && computerNumber < _computers.Count)
+                    {
+                        if (_computers[computerNumber].IsBusy)
+                        {
+                            Console.WriteLine("This computer has already busied");
+                        }
+                        else
+                        {
+                            if (schoolBoy.CheckSolvency(_computers[computerNumber]))
+                            {
+                                Console.WriteLine("Count the money, client paid for neccesary time and sat down at computer");
+                                _money += schoolBoy.ToPay();
+
+                                _computers[computerNumber].TakeThePlace(schoolBoy);
+                            }
+                            else
+                            {
+                                Console.WriteLine("Something went wrong");
+                            }
+                        }
+                    }
+                    else Console.WriteLine("We don't have such computer. Client is gone out.");
+
+                    Console.WriteLine("For going to a new client press any key");
+                    SkipMinutes();
+                }
+                catch
+                {
+                    Console.WriteLine("Number was not valid!!");
+                }
+                Console.ReadKey();
+                Console.Clear();
+
+
+            }
+        }
+
+        private void ShowAllComputers()
+        {
+            for (int i = 0; i < _computers.Count; i++)
+            {
+                Console.Write($"{i + 1} - ");
+                _computers[i].ShowComputersInfo();
+            }
+        }
+        public void SkipMinutes()
+        {
+            foreach (var compute in _computers)
+            {
+                compute.SkipMinutes();
+            }
+        }
     }
+
+
+
+
 
     class Computer
     {
@@ -55,7 +153,7 @@ namespace Computers_club
 
         public void ShowComputersInfo()
         {
-            if (IsBusy) Console.WriteLine($"Computer is busy. {_minutesLeft} are left");
+            if (IsBusy) Console.WriteLine($"Computer is busy. {_minutesLeft} minutes are left");
             else Console.WriteLine($"Computer is free. Price for minute equals {PriceForMinute}");
         }
     }
@@ -63,14 +161,31 @@ namespace Computers_club
     class SchoolBoy
     {
         private int _money;
+        private int _moneyToPay;
 
         public int DesiredMinutes { get; private set; }
 
-        public SchoolBoy(int money)
+        public SchoolBoy(int money, Random random)
         {
-            Random random = new Random();
             _money = money;
             DesiredMinutes = random.Next(10, 30);
+        }
+
+        public bool CheckSolvency(Computer computer)
+        {
+            _moneyToPay = computer.PriceForMinute * DesiredMinutes;
+            if (_money >= _moneyToPay) return true;
+            else
+            {
+                _moneyToPay = 0;
+                return false;
+            };
+        }
+
+        public int ToPay()
+        {
+            _money -= _moneyToPay;
+            return _moneyToPay;
         }
     }
 }
